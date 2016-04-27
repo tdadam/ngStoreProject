57bc01521e22fe4c6588a76a52f67e546abeb4a3
Tim Adam <tdadam@xactware.com>
2016-04-26 22:04:39 -0600

    Login link working, need to get authication hooked up

--- server.js
+++ server.js
@@ -1,16 +1,57 @@
 var express = require('express');
 var app = express();
+var bodyParse = require('body-parser');
 var fs = require('fs');
-var bodyParser = require('body-parser');
+var passport = require('passport')
+    , FacebookStrategy = require('passport-facebook').Strategy,
+    LocalStrategy = require('passport-local').Strategy;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({
+    extended: true
+}));
 
-//used for the get to pull file info
-var path = require('path');
+app.use('/', express.static(__dirname));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(express.static(__dirname));
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done){
+    done(null, obj);
+});
+
+passport.use('facebook', new FacebookStrategy({
+        clientID: '661695950638053',
+        clientSecret: '4084a4ffb47ccace28b52570ca12719d',
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        cb(null, profile);
+    }
+));
+
+app.get('/auth/facebook',
+    passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/fail' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+
+app.post('/api/login', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        console.log(err, user, info);
+        res.json(user);
+    })(req, res, next);
+});
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('App listening on port 3000...');
 });
